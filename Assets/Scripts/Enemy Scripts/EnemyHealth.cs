7169c1fb1bea1c197a11a21a26718cf4a50a720c
Alejandro <cesar.leo14@yahoo.com>
2018-03-31 18:09:42 -0500

    Final Menu Setup
    Completed the menu with the correct labels, images, controls and credits.

--- Assets/Scripts/Enemy Scripts/EnemyHealth.cs
+++ Assets/Scripts/Enemy Scripts/EnemyHealth.cs
@@ -7,17 +7,12 @@ public class EnemyHealth : MonoBehaviour
 {
     [SerializeField]
     private int startingEnemyHealth = 100;      // The amount of health the enemy starts the game with.
-    [SerializeField]
-    private float sinkSpeed = 2.5f;             // The speed at which the enemy sinks through the floor when dead.
-    [SerializeField]
-    private int scoreValue = 10;                // The amount added to the player's score when the enemy dies.
 
     [HideInInspector]
     public int currentEnemyHealth;              // The current health the enemy has.
 
     private BoxCollider boxCollider;            // Reference to the box collider.
     private bool isDead;                        // Whether the enemy is dead.
-    private bool isSinking;                     // Whether the enemy has started sinking through the floor.
 
 
 
@@ -28,16 +23,6 @@ public class EnemyHealth : MonoBehaviour
 
         // Setting the current health when the enemy first spawns.
         currentEnemyHealth = startingEnemyHealth;
-    }
-
-    void Update()
-    {
-        // If the enemy should be sinking...
-        if (isSinking)
-        {
-            // ... move the enemy down by the sinkSpeed per second.
-            transform.Translate(-Vector3.up * sinkSpeed * Time.deltaTime);
-        }
     }
 
 
@@ -64,9 +49,6 @@ public class EnemyHealth : MonoBehaviour
     {
         // The enemy is dead.
         isDead = true;
-
-        // Turn the collider into a trigger so shots can pass through it.
-        boxCollider.isTrigger = true;
     }
 
 
@@ -74,17 +56,5 @@ public class EnemyHealth : MonoBehaviour
     {
         // Find and disable the Nav Mesh Agent.
         GetComponent<NavMeshAgent>().enabled = false;
-
-        // Find the rigidbody component and make it kinematic (since we use Translate to sink the enemy).
-        GetComponent<Rigidbody>().isKinematic = true;
-
-        // The enemy should no sink.
-        isSinking = true;
-
-        // Increase the score by the enemy's score value.
-        //ScoreManager.score += scoreValue;
-
-        // After 2 seconds destory the enemy.
-        Destroy(gameObject, 2f);
     }
 }
--- Assets/Scripts/Menu Scripts/SelectOnInput.cs
+++ Assets/Scripts/Menu Scripts/SelectOnInput.cs
@@ -7,14 +7,13 @@ public class SelectOnInput : MonoBehaviour
 {
     public EventSystem eventSystem;
     public GameObject selectedObject;
-    public int playerNumber;
 
     private bool buttonSelected;
 
 
     private void Update()
     {
-        if (Input.GetAxisRaw("Vertical" + playerNumber) != 0 && buttonSelected == false)
+        if (Input.GetAxisRaw("Vertical") != 0 && buttonSelected == false)
         {
             eventSystem.SetSelectedGameObject(selectedObject);
             buttonSelected = true;
